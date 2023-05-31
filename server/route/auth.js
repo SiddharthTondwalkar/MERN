382d77d576cfe1408be6a6c7d026b83e104d7d8a
SiddharthTondwalkar <stondwalk19@gmail.com>
2023-06-01 00:24:58 +0530

    Added async in route

--- server/route/auth.js
+++ server/route/auth.js
@@ -1,42 +1,44 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
-require('../db/conn');
-const User = require('../model/userSchema')
+require("../db/conn");
+const User = require("../model/userSchema");
 
-const authMiddlware = (req, res, next) =>{
-    next()
-}
+const authMiddlware = (req, res, next) => {
+  next();
+};
 
 // authMiddlware();
-router.get('/', (req, res) => {
-    res.send('Hello World! from server')
-})
+router.get("/", (req, res) => {
+  res.send("Hello World! from server");
+});
 
-router.post('/register', (req, res) => {
-    // console.log(req.body);
-    // res.send({message: req.body})
-    const { name, email,number, password, cpassword } = req.body;
+router.post("/register", async (req, res) => {
+  const { name, email, number, password, cpassword } = req.body;
 
-    if(!name || !email || !number || !password || !cpassword){
-        return res.json({error:"Please Fill All The Fields"});
+  if (!name || !email || !number || !password || !cpassword) {
+    return res.json({ error: "Please Fill All The Fields" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.json({ error: "Email Already Exist" });
     }
+    const user = new User(req.body);
+    await user.save();
+    
+    return res.json({ message: "User Register Successfuly" });
+    
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "Failed To Register" });
+  }
+});
 
-    User.findOne({email:email}).then((userExist)=>{
-        if(userExist){
-        return res.json({error:"Email Already Exist"});
-        }
-        const user = new User(req.body)
-        user.save().then(()=>{
-        return res.json({message:"User Register Successfuly"});
-        }).catch(err=> {res.status(500).json({error:"Failed To Register"})})
-    }).catch(err => { console.log(err)});
-})
-
-
-router.get('/about',authMiddlware, (req, res) => {
-    res.send('Hello World!')
-  })
+router.get("/about", authMiddlware, (req, res) => {
+  res.send("Hello World!");
+});
 
 module.exports = router;
